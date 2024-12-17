e4bfcebe2ab16474875a8de0fa4ee9f6482eca39
Christoffer Hafskjold <christoffer.hafskjold@gmail.com>
2024-12-17 22:23:45 +0100

    Initial commit

---
+++ dropdown.js
@@ -0,0 +1,9 @@
+// Hide non-active objects and change the visibility if they are clicked:
+function toggleDropdown(button) {
+    const content = button.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
